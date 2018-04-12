3ee76272fd4d0bd156dcb4a5ffe2c66862daf652
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2018-04-12 23:37:06 +0300

    Fixed typo

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get current bootloader
-sudo dd if=/dev/sda1 of=/tmp/bootloader-current.img count=511 2>&1 status=none
+sudo dd if=/dev/sda1 of=/tmp/bootloader-current.img count=1 status=none
 
 # Compare byte-by-byte
 cmp --silent ./bootloader-orig.img /tmp/bootloader-current.img && \
