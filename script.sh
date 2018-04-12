6ed57340362a696fe4d31c2e2e66642b91171a57
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2018-04-12 23:08:43 +0300

    Initial commit. First version of script.

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get current bootloader
+sudo dd if=/dev/sda1 of=/tmp/bootloader-current.img count=511 2>&1 status=none
+
+# Compare byte-by-byte
+cmp --silent ./bootloader-orig.img /tmp/bootloader-current.img && echo "Bootloader is not modified" || echo "WARNING! Bootloader is modified!!!"
