01e165b7497f53d88f5618d39d4e5259e2d5f5d8
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2018-04-12 23:11:03 +0300

    Updated script. Added new line to more readable format

--- script.sh
+++ script.sh
@@ -4,4 +4,6 @@
 sudo dd if=/dev/sda1 of=/tmp/bootloader-current.img count=511 2>&1 status=none
 
 # Compare byte-by-byte
-cmp --silent ./bootloader-orig.img /tmp/bootloader-current.img && echo "Bootloader is not modified" || echo "WARNING! Bootloader is modified!!!"
+cmp --silent ./bootloader-orig.img /tmp/bootloader-current.img && \
+    echo "Bootloader is not modified" || \
+    echo "WARNING! Bootloader is modified!!!"
